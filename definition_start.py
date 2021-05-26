b2057e2adcc76f685b6d23adb3e595852cdb11a7
anudeepthota <anudeepsuresh@Anudeeps-MBP.grandenetworks.net>
2021-05-25 20:48:30 -0500

    OOP Commit 1

---
+++ definition_start.py
@@ -0,0 +1,16 @@
+# Basic class definitions
+
+# TODO: create a basic class
+class Book:
+    def __init__(self, title):
+        self.title = title
+
+#TODO: create instances of the class
+b1 = Book("My First Book")
+
+b2 = Book("My Second Book")
+
+
+# TODO: print the class and property
+print(b1)
+print(b1.title)
